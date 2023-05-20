3fe03aade9652283efae6a00a987b3498a7b7eeb
rmistr00 <115426122+rmistr00@users.noreply.github.com>
2023-05-20 00:56:45 +0000

    card link

--- src/components/bottom_bar.js
+++ src/components/bottom_bar.js
@@ -6,12 +6,42 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 
 function BottomBar() {
+  const [link, setLink] = useState("");
+
   return (
     <div className="bottom-bar">
+      {link && (
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className="link-card"
+        >
+          {link}
+        </motion.div>
+      )}
+
       <div className="links">
         {settings.links.map((x, n) => (
-          <div className="link" key={x.id}>
+          <div
+            className={`link ${link == x.id ? "selected-link" : ""}`}
+            key={x.id}
+            onClick={() => {
+              if (link == "" || link != x.id) {
+                setLink(x.id);
+              } else {
+                setLink("");
+              }
+            }}
+          >
             {x.id}
+            {
+              <div
+                className="social-img"
+                style={{
+                  backgroundImage: `url(https://github.com/rmistr00/japan-club/blob/main/src/assets/socials/${x.id}.png)`,
+                }}
+              ></div>
+            }
           </div>
         ))}
       </div>
